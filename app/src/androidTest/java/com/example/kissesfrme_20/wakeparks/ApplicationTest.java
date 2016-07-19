77bf33cd08883c8cdd9f59d5a31cc98c568cb261
iamkeeb <amanda.keebler@gmail.com>
2016-07-18 20:18:41 -0500

    first commit

---
+++ app/src/androidTest/java/com/example/kissesfrme_20/wakeparks/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.kissesfrme_20.wakeparks;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
