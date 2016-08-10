ab40b20c36bae849a815b9e546b73cc0e5553958
iamkeeb <amanda.keebler@gmail.com>
2016-08-10 11:18:16 -0500

    can logout

--- app/src/main/java/com/example/kissesfrme_20/wakeparks/ProfileActivity.java
+++ app/src/main/java/com/example/kissesfrme_20/wakeparks/ProfileActivity.java
@@ -5,14 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth firebaseAuth;
 
     Intent intent = getIntent();
+    private Button logout;
+    private String email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, login_page.class));
+        }
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        logout = (Button) findViewById(R.id.logoutbutton);
+        logout.setOnClickListener(this);
     }
 
     @Override
@@ -25,6 +44,15 @@ public class ProfileActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == logout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, login_page.class));
+        }
     }
 
     public void listParks(View view) {
